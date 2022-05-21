338c24de612c6ea4f461dbc1b4a9581161a072f2
Parker Thomson <latridell.services@gmail.com>
2022-05-20 20:13:24 -0400

    Worked on 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
 const removeFromArray = function() {
+    var Array1 = [];
+    if(arguments.length >= 1){
+        Array1 = Array.from(arguments[0]);
+        for(i = 1; i < Array1.length; i++){
+            console.log(i);
+            for(j = 1; j < arguments.length; j++){
+                
+            }
+        }
+    }else{
+        console.log("There are no arguments in the function");
+    }
 
 };
 
+removeFromArray([1,2,3,4,5]);
+
+
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
