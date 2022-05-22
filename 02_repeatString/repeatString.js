094f63025fd4d67062956bec31537bab7a1e3a85
Parker Thomson <parkercthomson@gmail.com>
2022-05-22 08:41:39 -0400

    DId exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    var final = '';
+    
+    for(num; num > 0; num--){
+        final = final.concat(string);
+    }
+    return final;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
