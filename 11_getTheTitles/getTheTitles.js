baa60195ffc7206d193257d50d099151de333391
Parker Thomson <parkercthomson@gmail.com>
2022-08-10 00:31:23 -0400

    FInished Get the titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,7 +1,17 @@
-const getTheTitles = function() {
+    const getTheTitles = function(ob) {
+        var array1 = new Array(0);
+        var len = Object.keys(ob).length;
 
-    
+
+
+         for(var j = 0; j < len; j++){
+            array1.push(ob[j].title);
+        }
+
+        return array1;
 };
+
+//Working function
 
 
 // Do not edit below this line
